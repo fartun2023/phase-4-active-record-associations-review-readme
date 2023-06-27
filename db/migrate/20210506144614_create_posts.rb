a88c5cc313beb6c0fe1af9f85620b366503814ca
fartun <fartunwario34@gmail.com>
2023-06-27 12:31:35 +0300

    completed assignment

--- db/migrate/20210506144614_create_posts.rb
+++ db/migrate/20210506144614_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :content
-      t.belongs_to :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210506151732_create_profiles.rb
+++ db/migrate/20210506151732_create_profiles.rb
@@ -2,12 +2,7 @@ class CreateProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :profiles do |t|
       t.string :username
-      t.string :email
-      t.string :bio
-      t.string :avatar_url
-      t.string :facebook
-      t.belongs_to :author, null: false, foreign_key: true
-
+      t.references :author, null: false, foreign_key: true
       t.timestamps
     end
   end
